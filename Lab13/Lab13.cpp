b7cfc0115915a4bdc1cdef8e7cbe1ffe3bc03dcd
Данил Кравченко <danil_kravchenko_2007@mail.ru>
2026-03-30 23:55:36 +0700

    считывание матрицы из файла и вывод ее в консоль

--- Lab13/Lab13.cpp
+++ Lab13/Lab13.cpp
@@ -1,20 +1,52 @@
-// Lab13.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿#include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	ifstream file("C:\\Users\\danilka\\Downloads\\tests.txt");
+	if (!file.is_open()) {
+		cout << "Ошибка открытия файла." << endl;
+		return 1;
+	}
+
+	int M, N; // кол-во уравнений (M) и кол-во неизвестных (N)
+
+	file >> M >> N;
+
+	double** matrix = new double* [M]; // выделяем память под матрицу
+	for (int i = 0; i < M; i++) {
+		matrix[i] = new double[N + 1];
+	}
+
+
+	for (int i = 0; i < M; i++) { // записываем данные из файла в наш двумерный массив
+		for (int j = 0; j <= N; j++) {
+			file >> matrix[i][j];
+		}
+	}
+
+	cout << "Исходная матрица:" << endl;
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j <= N; j++) {
+			cout << setw(8) << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < M; i++) { // очистка памяти
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+
+	file.close(); // закрытие файла
+
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
