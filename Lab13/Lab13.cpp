b78d4bd23f713f939d368130879a2754f87edfe6
Данил Кравченко <danil_kravchenko_2007@mail.ru>
2026-04-01 19:38:27 +0700

    реализация метода Гаусса-Жордана

--- Lab13/Lab13.cpp
+++ Lab13/Lab13.cpp
@@ -5,6 +5,45 @@
 
 using namespace std;
 
+void gaussMethod(double** matrix, int M, int N) {
+    
+    for (int k = 0; k < min(M, N); k++) { // проходимся по столбцам
+
+        // находим максимальный элемент в столбце
+        int maxRow = k;
+        for (int i = k + 1; i < M; i++) {
+            if (abs(matrix[i][k]) > abs(matrix[maxRow][k])) {
+                maxRow = i;
+            }
+        }
+
+        // меняем местами текущую строку и строку с максимальным элементом в столбце
+        if (maxRow != k) {
+            swap(matrix[k], matrix[maxRow]);
+        }
+
+        // если ведущий элемент равен нулю, значит, в этом столбце нет решений
+        if (abs(matrix[k][k]) < 1e-9) continue;
+
+        // запоминаем ведущий элемент
+        double pivot = matrix[k][k];
+
+        // делим строку на ведущий элемент, чтоб на диагонали получить 1
+        for (int j = k; j <= N; j++) {
+            matrix[k][j] /= pivot;
+        }
+
+        // обнуляем все элементы в столбце k выше и ниже главной диагонали
+        for (int i = 0; i < M; i++) {
+            if (i != k) { // пропускаем текущую строку
+                double factor = matrix[i][k]; // множитель
+                for (int j = k; j <= N; j++) {
+                    matrix[i][j] -= factor * matrix[k][j];
+                }
+            }
+        }
+    }
+}
 
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -39,6 +78,17 @@ int main() {
 		}
 		cout << endl;
 	}
+
+    gaussMethod(matrix, M, N);
+
+    cout << "\nМатрица после Гаусса-Жордана:" << endl;
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j <= N; j++) {
+            if (abs(matrix[i][j]) < 1e-9) matrix[i][j] = 0;
+            cout << setw(8) << fixed << setprecision(3) << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 
 	for (int i = 0; i < M; i++) { // очистка памяти
 		delete[] matrix[i];
